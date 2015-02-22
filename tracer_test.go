e7497766897ad0c0439535fe9ad74411a2603454
Isaac Ross <isaac253@gmail.com>
2015-02-22 09:52:07 -0500

    Trace interface and New method

---
+++ tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Return from New should not be nil")
+	} else {
+		tracer.Trace("Hello from trace package.")
+		if buf.String() != "Hello from trace package.\n" {
+			t.Errorf("Trace should not write '%s'.", buf.String())
+		}
+	}
+}
