aa04b33b83fa459d9dadff7640217c0c561b2236
Isaac Ross <isaac253@gmail.com>
2015-02-22 15:23:10 -0500

    updating doc

--- tracer.go
+++ tracer.go
@@ -5,16 +5,18 @@ import (
 	"io"
 )
 
-//Tracer is the interface that describes an object capable of tracing
-// events trhoughout code
-
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code.
 type Tracer interface {
 	Trace(...interface{})
 }
 
+// tracer is a Tracer that writes to an io.Writer.
 type tracer struct {
 	out io.Writer
 }
+
+// nilTracer
 type nilTracer struct{}
 
 func (t *nilTracer) Trace(a ...interface{}) {}
@@ -24,10 +26,13 @@ func (t *tracer) Trace(a ...interface{}) {
 	t.out.Write([]byte("\n"))
 }
 
+// New creates a new Tracer that will write the output to
+// the specified io.Writer
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
 }
 
+// Off creates a tracer that will ignore all calls to Trace
 func Off() Tracer {
 	return &nilTracer{}
 }
